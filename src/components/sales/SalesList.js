9da9d9d7fe49ccfdbdd7bf0fa82f76f1e0f61bf8
Drushti R Shetty <dshetty@primesoft.local>
2023-07-18 23:09:07 +0530

    second commit

--- src/components/sales/SalesList.js
+++ src/components/sales/SalesList.js
@@ -1,59 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useFetchSalesQuery } from '../../store/salesApi';
 import SalesCard from './SalesCard';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 const SalesList = () => {
-  const { data: sales, isLoading, isError, fetchNextPage, hasNextPage, error } = useFetchSalesQuery();
+  const [renderedSales, setRenderedSales] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data: sales, isLoading, isError, error } = useFetchSalesQuery({ pageParam: currentPage });
 
-  const renderedSales = sales || [];
-  console.log(renderedSales);
+  useEffect(() => {
+    if (sales) {
+      setRenderedSales((prevSales) => [...prevSales, ...sales]);
+    }
+  }, [sales]);
+
   const handleLoadMore = () => {
-    fetchNextPage();
+    setCurrentPage((prevPage) => prevPage + 1);
   };
-  if (isLoading) {
 
+  if (isLoading && renderedSales.length === 0) {
     return <div>Loading...</div>;
-
-  } else if (error) {
-
-    return <div>Error fetching data...</div>;
-
+  } else if (isError) {
+    return <div>Error fetching data: {error.message}</div>;
   } else {
-  return (
-  <div> <div className="sales-list">
-  {renderedSales.map((sale, index) => (
-    <SalesCard
-      key={index}
-      country={sale.country}
-      sales={sale.sales}
-      value={sale.value}
-      bounce={sale.bounce}
-    />
-  ))}
-  {!isLoading && renderedSales.length >= 10 && (
-    <InfiniteScroll
-      dataLength={renderedSales.length}
-      next={handleLoadMore}
-      hasMore={hasNextPage}
-      loader={<p>Loading...</p>}
-      scrollableTarget="sales-list"
-      scrollThreshold={0.8}
-    >
-      {renderedSales.slice(10).map((sale, index) => (
-        <SalesCard
-          key={index + 10}
-          country={sale.country}
-          sales={sale.sales}
-          value={sale.value}
-          bounce={sale.bounce}
-        />
-      ))}
-    </InfiniteScroll>
-  )}
-</div></div>
-  );
-          }
+    return (
+      <div className="sales-list">
+        {renderedSales.map((sale, index) => (
+          <SalesCard key={index} country={sale.country} sales={sale.sales} value={sale.value} bounce={sale.bounce} />
+        ))}
+        {sales && (
+          <InfiniteScroll
+            dataLength={renderedSales.length}
+            next={handleLoadMore}
+            hasMore={sales.length > 0}
+            loader={<p>Loading...</p>}
+            scrollableTarget="sales-list"
+            scrollThreshold={0.8}
+          />
+        )}
+      </div>
+    );
+  }
 };
 
 export default SalesList;
